d31241b1753e1ea57f5ae60ef4a45f1da10cadd5
kirky359 <lol783176@gmail.com>
2025-09-06 16:47:57 +0300

    fix

--- src/App.jsx
+++ src/App.jsx
@@ -18,6 +18,18 @@ export const goods = [
 export const App = () => {
   const [selectedGood, setSelectedGood] = useState('Jam');
 
+  function handleClearSelection() {
+    setSelectedGood('');
+  }
+
+  function handleRemoveSelection() {
+    setSelectedGood('');
+  }
+
+  function handleSelectGood(good) {
+    setSelectedGood(good);
+  }
+
   return (
     <main className="section container">
       {selectedGood ? (
@@ -27,7 +39,7 @@ export const App = () => {
             data-cy="ClearButton"
             type="button"
             className="delete ml-3"
-            onClick={() => setSelectedGood('')}
+            onClick={handleClearSelection}
           />
         </h1>
       ) : (
@@ -52,7 +64,7 @@ export const App = () => {
                     data-cy="RemoveButton"
                     type="button"
                     className="button is-info"
-                    onClick={() => setSelectedGood('')}
+                    onClick={handleRemoveSelection}
                   >
                     -
                   </button>
@@ -61,7 +73,7 @@ export const App = () => {
                     data-cy="AddButton"
                     type="button"
                     className="button"
-                    onClick={() => setSelectedGood(good)}
+                    onClick={() => handleSelectGood(good)}
                   >
                     +
                   </button>
